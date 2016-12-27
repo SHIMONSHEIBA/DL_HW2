eaf9ef744e76099cd92a9d8f4aefa6c294b9854a
SHIMONSHEIBA <shimonsheiba@gmail.com>
2016-12-27 20:58:36 +0200

    Update cifar10_classsification.lua

--- cifar10_classsification.lua
+++ cifar10_classsification.lua
@@ -108,17 +108,17 @@ end
 local model = nn.Sequential()
 model:add(nn.BatchFlip():float())
 --model:add(cudnn.SpatialConvolution(3, 32, 5, 5)) -- 3 input image channel, 32 output channels, 5x5 convolution kernel
-model:add(cudnn.SpatialConvolution(3, 32, 5, 5)) -- 3 input image channel, 32 output channels, 5x5 convolution kernel
---model:add(cudnn.SpatialBatchNormalization(32))    --Batch normalization will provide quicker convergence
---model:add(nn.LeakyReLU(true))                          -- ReLU activation function
---model:add(cudnn.SpatialMaxPooling(2,2,2,2))      -- A max-pooling operation that looks at 2x2 windows and finds the max.
+model:add(cudnn.SpatialConvolution(3, 32, 5, 5,1,1,2,2)) -- 3 input image channel, 32 output channels, 5x5 convolution kernel
+model:add(cudnn.SpatialBatchNormalization(32))    --Batch normalization will provide quicker convergence
+model:add(nn.LeakyReLU(true))                          -- ReLU activation function
+model:add(cudnn.SpatialMaxPooling(2,2,2,2))      -- A max-pooling operation that looks at 2x2 windows and finds the max.
 --model:add(nn.SpatialMaxPooling(2,2,2,2))      -- A max-pooling operation that looks at 2x2 windows and finds the max.
 --model:add(cudnn.ReLU(true))                          -- ReLU activation function
-model:add(cudnn.SpatialConvolution(32, 32, 5, 5))
---model:add(cudnn.SpatialBatchNormalization(32))    --Batch normalization will provide quicker convergence
---model:add(nn.LeakyReLU(true))                          -- ReLU activation function
+model:add(cudnn.SpatialConvolution(32, 32, 5, 5,1,1,2,2))
+model:add(cudnn.SpatialBatchNormalization(32))    --Batch normalization will provide quicker convergence
+model:add(nn.LeakyReLU(true))                          -- ReLU activation function
 --model:add(cudnn.SpatialConvolution(32, 64, 3, 3))
---model:add(cudnn.SpatialMaxPooling(2,2,2,2))
+model:add(cudnn.SpatialMaxPooling(2,2,2,2))
 --model:add(nn.Dropout(0.2)) 
 --model:add(cudnn.SpatialConvolution(16, 16, 5, 5, 1, 1, 2, 2))
 --model:add(cudnn.SpatialMaxPooling(2,2,2,2))
@@ -126,17 +126,17 @@ model:add(cudnn.SpatialConvolution(32, 32, 5, 5))
 --model:add(cudnn.SpatialBatchNormalization(16))
 --model:add(nn.LeakyReLU(true))
 --model:add(cudnn.SpatialConvolution(64, 32, 3, 3))
---model:add(cudnn.SpatialConvolution(32, 16, 5, 5, 2, 2, 2, 2))
---model:add(nn.View(16*4*4):setNumInputDims(3))  -- reshapes from a 3D tensor of 32x4x4 into 1D tensor of 32*4*4
---model:add(nn.Linear(16*4*4, 32))             -- fully connected layer (matrix multiplication between input and weights)
+model:add(cudnn.SpatialConvolution(32, 16, 5, 5, 2, 2, 2, 2))
+model:add(nn.View(16*4*4):setNumInputDims(3))  -- reshapes from a 3D tensor of 32x4x4 into 1D tensor of 32*4*4
+model:add(nn.Linear(16*4*4, 32))             -- fully connected layer (matrix multiplication between input and weights)
 --model:add(cudnn.ReLU(true))
---model:add(nn.LeakyReLU(true))
---model:add(nn.Dropout(0.4))                      --Dropout layer with p=0.2
---model:add(nn.Linear(32, #classes))            -- 10 is the number of outputs of the network (in this case, 10 digits)
---model:add(nn.LogSoftMax())                     -- converts the output to a log-probability. Useful for classificati
+model:add(nn.LeakyReLU(true))
+model:add(nn.Dropout(0.4))                      --Dropout layer with p=0.2
+model:add(nn.Linear(32, #classes))            -- 10 is the number of outputs of the network (in this case, 10 digits)
+model:add(nn.LogSoftMax())                     -- converts the output to a log-probability. Useful for classificati
 
---model:cuda()
---criterion = nn.ClassNLLCriterion():cuda()
+model:cuda()
+criterion = nn.ClassNLLCriterion():cuda()
 
 
 w, dE_dw = model:getParameters()
