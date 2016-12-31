1e5ccf0418ef00bb8f5a2619a26dc9a5c48e0157
reutapel <reutapel88@gmail.com>
2016-12-31 15:35:59 +0200

    Update cifar10_classsification.lua

--- cifar10_classsification.lua
+++ cifar10_classsification.lua
@@ -127,6 +127,9 @@ model:add(cudnn.SpatialMaxPooling(2,2,2,2))
 --model:add(nn.LeakyReLU(true))
 --model:add(cudnn.SpatialConvolution(64, 32, 3, 3))
 model:add(cudnn.SpatialConvolution(32, 16, 5, 5, 2, 2, 2, 2))
+model:add(cudnn.SpatialBatchNormalization(16))    --Batch normalization will provide quicker convergence
+model:add(nn.LeakyReLU(true))  
+model:add(cudnn.SpatialMaxPooling(2,2,2,2))
 model:add(nn.View(#classes))  -- reshapes from a 3D tensor of 32x4x4 into 1D tensor of 32*4*4
 --model:add(nn.Linear(16*4*4, 32))             -- fully connected layer (matrix multiplication between input and weights)
 --model:add(cudnn.ReLU(true))
