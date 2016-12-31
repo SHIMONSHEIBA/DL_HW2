c2a56866a8bf8287c1a7806495a22f2c1ac2ca42
reutapel <reutapel88@gmail.com>
2016-12-31 15:23:33 +0200

    Update cifar10_classsification.lua

--- cifar10_classsification.lua
+++ cifar10_classsification.lua
@@ -108,13 +108,13 @@ end
 local model = nn.Sequential()
 model:add(nn.BatchFlip():float())
 --model:add(cudnn.SpatialConvolution(3, 32, 5, 5)) -- 3 input image channel, 32 output channels, 5x5 convolution kernel
-model:add(cudnn.SpatialConvolution(3, 128, 5, 5,1,1,2,2)) -- 3 input image channel, 32 output channels, 5x5 convolution kernel
-model:add(cudnn.SpatialBatchNormalization(128))    --Batch normalization will provide quicker convergence
+model:add(cudnn.SpatialConvolution(3, 64, 5, 5,1,1,2,2)) -- 3 input image channel, 32 output channels, 5x5 convolution kernel
+model:add(cudnn.SpatialBatchNormalization(64))    --Batch normalization will provide quicker convergence
 model:add(nn.LeakyReLU(true))                          -- ReLU activation function
 model:add(cudnn.SpatialMaxPooling(2,2,2,2))      -- A max-pooling operation that looks at 2x2 windows and finds the max.
 --model:add(nn.SpatialMaxPooling(2,2,2,2))      -- A max-pooling operation that looks at 2x2 windows and finds the max.
 --model:add(cudnn.ReLU(true))                          -- ReLU activation function
-model:add(cudnn.SpatialConvolution(128, 32, 5, 5,1,1,2,2))
+model:add(cudnn.SpatialConvolution(64, 32, 5, 5,1,1,2,2))
 model:add(cudnn.SpatialBatchNormalization(32))    --Batch normalization will provide quicker convergence
 model:add(nn.LeakyReLU(true))                          -- ReLU activation function
 --model:add(cudnn.SpatialConvolution(32, 64, 3, 3))
