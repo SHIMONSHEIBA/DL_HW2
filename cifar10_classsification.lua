47831b4d23348d7437a3965b7b8c11519cf7f56d
reutapel <reutapel88@gmail.com>
2017-01-01 17:03:49 +0200

    Update cifar10_classsification.lua

--- cifar10_classsification.lua
+++ cifar10_classsification.lua
@@ -60,10 +60,10 @@ do -- data augmentation module
       local bs = input:size(1)
       local flip_mask = torch.randperm(bs)--:le(bs/2)
       for i=1,input:size(1) do
-       	if (flip_mask[i] % 6 == 0) then image.hflip(self.output[i], input[i]) end
-	if (flip_mask[i] % 6 == 1) then image.vflip(self.output[i], input[i]) end
+       	if (flip_mask[i] % 2 == 0) then image.hflip(self.output[i], input[i]) end
+	--if (flip_mask[i] % 6 == 1) then image.vflip(self.output[i], input[i]) end
 	--if (flip_mask[i] % 6 == 2) then image.RandomCrop(input[i],tl,32,32) end
-	if (flip_mask[i] % 6 == 3) then image.rotate(self.output[i], input[i],1.57079633) end
+	--if (flip_mask[i] % 6 == 3) then image.rotate(self.output[i], input[i],1.57079633) end
 	--if (flip_mask[i] % 6 == 4) then image.minmax(input[i]) end
     end
     end
