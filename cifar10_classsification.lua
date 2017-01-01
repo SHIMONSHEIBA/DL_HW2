de95f48532d7c73eadf95f7a00f6812930ab9853
reutapel <reutapel88@gmail.com>
2017-01-01 15:32:59 +0200

    Update cifar10_classsification.lua

--- cifar10_classsification.lua
+++ cifar10_classsification.lua
@@ -63,7 +63,7 @@ do -- data augmentation module
 	if (flip_mask[i] % 6 == 1) then image.vflip(input[i]) end
 	--if (flip_mask[i] % 6 == 2) then image.RandomCrop(input[i],tl,32,32) end
 	if (flip_mask[i] % 6 == 3) then image.rotate(input[i],1.57079633) end
-	if (flip_mask[i] % 6 == 4) then image.minmax(input[i]) end
+	--if (flip_mask[i] % 6 == 4) then image.minmax(input[i]) end
     end
     end
     self.output:set(input:cuda())
