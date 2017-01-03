27e1ca4f6b919831779c663dcf6fb6e9a680baf8
reutapel <reutapel88@gmail.com>
2017-01-03 23:26:13 +0200

    Update nin_try.lua

--- nin_try.lua
+++ nin_try.lua
@@ -21,30 +21,6 @@ local testLabels = testset.label:float():add(1)
 
 print(trainData:size())
 
---do -- data augmentation module
---  local BatchFlip,parent = torch.class('nn.BatchFlip', 'nn.Module')
-
---  function BatchFlip:__init()
---    parent.__init(self)
---    self.train = true
---  end
-
---  function BatchFlip:updateOutput(input)
---    if self.train then
---      local bs = input:size(1)
---      local flip_mask = torch.randperm(bs):le(bs/2)
---      for i=1,input:size(1) do
---        if flip_mask[i] == 1 then image.hflip(input[i], input[i]) end
---      end
---    end
---    self.output:set(input)
---    return self.output
---  end
---end
-
---local function horizontal_reflection(x)
---    return image.hflip(x)
---end
 -------------------added data augmantation----------------------------------
 do -- data augmentation module
   local BatchFlip,parent = torch.class('nn.BatchFlip', 'nn.Module')
@@ -56,14 +32,10 @@ do -- data augmentation module
 
   function BatchFlip:updateOutput(input)
     if self.train then
-			--print("andrey")
-      --self.output:set(input:cuda())
-			--self.output = input
-			--self.output:set(input)
       local bs = input:size(1)
-      local flip_mask = torch.randperm(bs)--:le(bs/2)
+      local flip_mask = torch.randperm(bs)
       for i=1, bs do
-       	--if (flip_mask[i] % 3 == 0) then image.hflip(input[i],input[i]) end
+       	if (flip_mask[i] % 3 == 0) then image.hflip(input[i],input[i]) end
 	--if (flip_mask[i] % 3 == 1) then image.vflip(input[i],input[i]) end
 	--if (flip_mask[i] % 3 == 1) then image.vflip(input[i],input[i]) end
 	--if (flip_mask[i] % 6 == 2) then image.RandomCrop(input[i],tl,32,32) end
