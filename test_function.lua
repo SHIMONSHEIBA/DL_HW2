21208c5ee7344d58fe538047a1178c942c18fb57
reutapel <reutapel88@gmail.com>
2017-01-03 23:28:35 +0200

    Update test_function.lua

--- test_function.lua
+++ test_function.lua
@@ -17,27 +17,6 @@ trainLabels = trainset.label:float():add(1)
 testData = testset.data:float()
 testLabels = testset.label:float():add(1)
 
-do -- data augmentation module
-  local BatchFlip,parent = torch.class('nn.BatchFlip', 'nn.Module')
-
-  function BatchFlip:__init()
-    parent.__init(self)
-    self.train = true
-  end
-
-  function BatchFlip:updateOutput(input)
-    if self.train then
-      local bs = input:size(1)
-      local flip_mask = torch.randperm(bs)
-      for i=1, bs do
-       	if (flip_mask[i] % 3 == 0) then image.hflip(input[i],input[i]) end
-    end
-    end
-    self.output:set(input:cuda())
-    return self.output
-  end
-end
-
 --normalizing our data
 local redChannel = trainData[{ {}, {1}, {}, {}  }] -- this picks {all images, 1st channel, all vertical pixels, all horizontal pixels}
 
