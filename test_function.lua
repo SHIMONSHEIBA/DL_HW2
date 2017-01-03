b8ea5f2b4e7c65e5e6d4aad2f35e4786f8f01186
reutapel <reutapel88@gmail.com>
2017-01-03 22:31:55 +0200

    Update test_function.lua

--- test_function.lua
+++ test_function.lua
@@ -1,26 +1,48 @@
-local mnist = require 'mnist';
-require 'nn'
 require 'cunn'
 require 'optim'
+require 'torch'
+require 'nn'
+require 'cudnn'
 
-model = torch.load('ClassifierModel.t7')
+model = torch.load('ConvClassifierModel8_2.t7')
 
-local trainData = mnist.traindataset().data:float();
-testData = mnist.testdataset().data:float();
-testLabels = mnist.testdataset().label:add(1);
+local trainset = torch.load('cifar.torch/cifar10-train.t7')
+local testset = torch.load('cifar.torch/cifar10-test.t7')
+
+local classes = {'airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'}
+
+local trainData = trainset.data:float() -- convert the data from a ByteTensor to a float Tensor.
+local trainLabels = trainset.label:float():add(1)
+local testData = testset.data:float()
+local testLabels = testset.label:float():add(1)
 
 --normalizing our data
-local mean = trainData:mean()
-local std = trainData:std()
-trainData:add(-mean):div(std); 
-testData:add(-mean):div(std);
+local redChannel = trainData[{ {}, {1}, {}, {}  }] -- this picks {all images, 1st channel, all vertical pixels, all horizontal pixels}
+print(#redChannel)
+
+local mean = {}  -- store the mean, to normalize the test set in the future
+local stdv  = {} -- store the standard-deviation for the future
+for i=1,3 do -- over each image channel
+    mean[i] = trainData[{ {}, {i}, {}, {}  }]:mean() -- mean estimation
+    print('Channel ' .. i .. ', Mean: ' .. mean[i])
+    trainData[{ {}, {i}, {}, {}  }]:add(-mean[i]) -- mean subtraction
+    
+    stdv[i] = trainData[{ {}, {i}, {}, {}  }]:std() -- std estimation
+    print('Channel ' .. i .. ', Standard Deviation: ' .. stdv[i])
+    trainData[{ {}, {i}, {}, {}  }]:div(stdv[i]) -- std scaling
+end
+
+for i=1,3 do -- over each image channel
+    testData[{ {}, {i}, {}, {}  }]:add(-mean[i]) -- mean subtraction    
+    testData[{ {}, {i}, {}, {}  }]:div(stdv[i]) -- std scaling
+end
 
 function TestModel()
 	
-	local confusion = optim.ConfusionMatrix(torch.range(0,9):totable())
+	local confusion = optim.ConfusionMatrix(classes)
 	local lossAcc = 0
 	local numBatches = 0
-	local batchSize = 16
+	local batchSize = 32
 	
 	criterion = nn.CrossEntropyCriterion():cuda()
 	
@@ -28,13 +50,12 @@ function TestModel()
 	
     for i = 1, testData:size(1) - batchSize, batchSize do
         numBatches = numBatches + 1
-        local x = testData:narrow(1, i, batchSize):cuda()
-        local yt = testLabels:narrow(1, i, batchSize):cuda()
+        local x = testData:narrow(1, i, batchSize)
+        local yt = testLabels:narrow(1, i, batchSize)
         local y = model:forward(x)
         local err = criterion:forward(y, yt)
         lossAcc = lossAcc + err
         confusion:batchAdd(y,yt)
-        
     end
     
     confusion:updateValids()
